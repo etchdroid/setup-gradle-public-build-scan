e50ec9f3facefadd2d94661aa82aa51fa517517a
Benjamin Muschko <benjamin.muschko@gmail.com>
2024-01-06 19:13:14 -0700

    Make plugin version configurable

--- src/gradle-build-scan.ts
+++ src/gradle-build-scan.ts
@@ -15,7 +15,7 @@ export async function setup(): Promise<void> {
     const initScriptFile = path.join(initScriptDir, 'build-scan.gradle')
 
     if (!fs.existsSync(initScriptFile)) {
-        await writeInitScript(initScriptFile, core.getInput('gradleEnterprisePluginVersion'))
+        await writeInitScript(initScriptFile, core.getInput('gradle-enterprise-plugin-version'))
     } else {
         core.error(`The initializing script '${initScriptFile}' already exists. Skipped creation!`)
     }
