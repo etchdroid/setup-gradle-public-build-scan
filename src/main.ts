41c3b48e069c2a43935cc1aa24c310072843e6c8
Benjamin Muschko <benjamin.muschko@gmail.com>
2024-01-10 18:44:46 -0700

    Use inputs to determine supported build tools

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import * as core from '@actions/core'
 
 import * as buildEnv from './build-env'
 import * as gradleBuildScan from './gradle-build-scan'
+import * as mavenBuildScan from './maven-build-scan'
 
 export async function run(): Promise<void> {
     try {
@@ -9,6 +10,9 @@ export async function run(): Promise<void> {
 
         if (buildTools.includes(buildEnv.BuildTool.Gradle)) {
             gradleBuildScan.setup()
+        }
+        if (buildTools.includes(buildEnv.BuildTool.Maven)) {
+            mavenBuildScan.setup()
         }
     } catch (error) {
         core.setFailed(String(error))
