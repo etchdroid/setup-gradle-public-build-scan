caef154eed6630481493915e27c38ed40463fa84
Benjamin Muschko <benjamin.muschko@gmail.com>
2024-01-13 16:43:38 -0700

    Add common user data plugin

--- src/gradle-build-scan.ts
+++ src/gradle-build-scan.ts
@@ -5,8 +5,8 @@ import * as lodash from 'lodash'
 import * as core from '@actions/core'
 import {readResourceFileAsString} from './build-env'
 
-const DEVELOCITY_PLUGIN_VERSION_INPUT ='develocity-gradle-plugin-version'
-const COMMON_USER_DATA_PLUGIN_VERSION_INPUT ='common-user-data-plugin-version'
+const DEVELOCITY_PLUGIN_VERSION_INPUT = 'develocity-gradle-plugin-version'
+const COMMON_USER_DATA_PLUGIN_VERSION_INPUT = 'common-user-data-plugin-version'
 const INIT_SCRIPT_PATH_OUTPUT = 'init-script-path'
 
 export async function setup(): Promise<void> {
@@ -20,16 +20,27 @@ export async function setup(): Promise<void> {
     const initScriptFile = path.join(initScriptDir, 'build-scan.gradle')
 
     if (!fs.existsSync(initScriptFile)) {
-        await writeInitScript(initScriptFile, core.getInput(DEVELOCITY_PLUGIN_VERSION_INPUT), core.getInput(COMMON_USER_DATA_PLUGIN_VERSION_INPUT))
+        await writeInitScript(
+            initScriptFile,
+            core.getInput(DEVELOCITY_PLUGIN_VERSION_INPUT),
+            core.getInput(COMMON_USER_DATA_PLUGIN_VERSION_INPUT)
+        )
         core.setOutput(INIT_SCRIPT_PATH_OUTPUT, initScriptFile)
     } else {
         core.error(`The Gradle initializing script '${initScriptFile}' already exists. Skipped creation!`)
     }
 }
 
-async function writeInitScript(initScriptFile: string, gradleEnterprisePluginVersion: string, commonUserDataPluginVersion: string): Promise<void> {
+async function writeInitScript(
+    initScriptFile: string,
+    gradleEnterprisePluginVersion: string,
+    commonUserDataPluginVersion: string
+): Promise<void> {
     const buildScanTemplate = readResourceFileAsString('build-scan.gradle')
-    const templateVars = {gradleEnterprisePluginVersion: gradleEnterprisePluginVersion, commonUserDataPluginVersion: commonUserDataPluginVersion}
+    const templateVars = {
+        gradleEnterprisePluginVersion: gradleEnterprisePluginVersion,
+        commonUserDataPluginVersion: commonUserDataPluginVersion
+    }
     lodash.templateSettings.interpolate = /\${([\s\S]+?)}/g
     const compiled = lodash.template(buildScanTemplate)
     const content = compiled(templateVars)
