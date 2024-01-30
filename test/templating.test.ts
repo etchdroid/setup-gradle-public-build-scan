b142301bb977f7a20d94e6bae92b25da8a841ac5
Benjamin Muschko <benjamin.muschko@gmail.com>
2024-01-29 18:18:31 -0700

    Add test for templating

---
+++ test/templating.test.ts
@@ -0,0 +1,19 @@
+import * as path from 'path'
+import * as os from 'os'
+import * as fs from 'fs'
+import * as tmp from 'tmp-promise'
+import {writeInitScript} from '../src/templating'
+
+describe('generate init script', () => {
+    it('with all values', async () => {
+        const tmpDirectory = await tmp.dir({
+          unsafeCleanup: true,
+        });
+        const initScriptFile = path.join(tmpDirectory.path, 'init.gradle')
+        writeInitScript(initScriptFile, '3.16.1', '1.12.1')
+        expect(fs.existsSync(initScriptFile)).toBe(true)
+        const initScriptContent = fs.readFileSync(initScriptFile, 'utf8')
+        expect(initScriptContent).toContain('com.gradle:gradle-enterprise-gradle-plugin:3.16.1')
+        expect(initScriptContent).toContain('com.gradle:common-custom-user-data-gradle-plugin:1.12.1')
+    })
+})
