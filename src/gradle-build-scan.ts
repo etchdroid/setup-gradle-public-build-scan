9eb5fa570d270fb6af31949ae275f9e9b7a54fac
Benjamin Muschko <benjamin.muschko@gmail.com>
2024-01-29 18:05:28 -0700

    Break out templating logic

--- src/gradle-build-scan.ts
+++ src/gradle-build-scan.ts
@@ -1,14 +1,13 @@
 import * as path from 'path'
 import * as fs from 'fs'
-import * as lodash from 'lodash'
 import * as core from '@actions/core'
-import {determineInitScriptDir, readResourceFileAsString} from './build-env'
+import {determineInitScriptDir} from './build-env'
+import {writeInitScript} from './templating'
 
 const DEVELOCITY_PLUGIN_VERSION_INPUT = 'develocity-gradle-plugin-version'
 const COMMON_USER_DATA_PLUGIN_VERSION_INPUT = 'common-user-data-plugin-version'
 const INIT_SCRIPT_PATH_OUTPUT = 'init-script-path'
 const INIT_SCRIPT_TARGET_FILENAME = 'build-scan.gradle'
-const INIT_SCRIPT_TEMPLATE_FILENAME = 'build-scan-template.gradle'
 
 export async function setup(): Promise<void> {
     const initScriptDir = determineInitScriptDir()
@@ -30,20 +29,4 @@ export async function setup(): Promise<void> {
     } else {
         core.error(`The Gradle initializing script '${initScriptFile}' already exists. Skipped creation!`)
     }
-}
-
-async function writeInitScript(
-    initScriptFile: string,
-    gradleEnterprisePluginVersion: string,
-    commonUserDataPluginVersion: string
-): Promise<void> {
-    const buildScanTemplate = readResourceFileAsString(INIT_SCRIPT_TEMPLATE_FILENAME)
-    const templateVars = {
-        gradleEnterprisePluginVersion,
-        commonUserDataPluginVersion
-    }
-    lodash.templateSettings.interpolate = /\${([\s\S]+?)}/g
-    const compiled = lodash.template(buildScanTemplate)
-    const content = compiled(templateVars)
-    fs.writeFileSync(initScriptFile, content)
 }
